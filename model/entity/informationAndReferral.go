c4d260e93a775993ca1a55ca401b0d0d12cdc28b
CadeWasti <cadewasti@gmail.com>
2026-02-05 17:56:30 -0600

    I&R all fields draft(?) #1

--- model/entity/informationAndReferral.go
+++ model/entity/informationAndReferral.go
@@ -8,9 +8,37 @@ import (
 
 type InformationAndReferral struct {
 	Id          uuid.UUID `gorm:"primaryKey;type:char(36)" json:"id"`
-	InfoType    string    `gorm:"size:100;not null" json:"infoType"`
-	Description string    `gorm:"size:255" json:"description"`
 	Date        time.Time `gorm:"type:date" json:"date"`
+	Hours       int64     `gorm:"type:bigint" json:"hours"`
+	TravelHours int64     `gorm:"type:bigint" json:"travelHours"`
+	Department  string    `gorm:"type:varchar(255)" json:"department"`
 
-	PersonId uuid.UUID `gorm:"not null" json:"personId"`
+	//Personal Intake Information
+	Name      string `gorm:"type:varchar(255)" json:"name"`
+	Address   string `gorm:"type:varchar(255)" json:"address"`
+	City      string `gorm:"type:varchar(255)" json:"city"`
+	State     string `gorm:"type:varchar(255)" json:"state"`
+	Zip       int64  `gorm:"type:bigint" json:"zip"`
+	Phone     string `gorm:"type:varchar(255)" json:"phone"`
+	County    string `gorm:"type:varchar(255)" json:"county"`
+	Email     string `gorm:"type:varchar(255)" json:"email"`
+	Gender    string `gorm:"type:varchar(255)" json:"gender"`
+	Ethnicity string `gorm:"type:varchar(255)" json:"ethnicity"`
+
+	//Referral and Requests
+	CallerType     string `gorm:"type:varchar(255)" json:"callerType"`
+	Disability     string `gorm:"type:varchar(255)" json:"disability"`
+	Referrer       string `gorm:"type:varchar(255)" json:"referrer"`
+	ServiceRequest string `gorm:"type:varchar(255)" json:"serviceRequest"`
+	Outcome        string `gorm:"type:varchar(255)" json:"outcome"`
+
+	//Logging
+	FormDate    time.Time `gorm:"type:date" json:"formDate"`
+	ServiceType string    `gorm:"type:varchar(255)" json:"serviceType"`
+	Grant       string    `gorm:"type:varchar(255)" json:"grant"`
+	Units       string    `gorm:"type:varchar(255)" json:"units"`
+
+	EmployeeId     uuid.UUID `gorm:"type:char(36)" json:"employeeId"`
+	OrganizationId uuid.UUID `gorm:"type:char(36)" json:"organizationId"`
+	PersonId       uuid.UUID `gorm:"not null" json:"personId"`
 }
