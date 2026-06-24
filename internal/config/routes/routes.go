cd4a0ca6365dc0d62d401a1297a4d33de7fb0a5b
Kyle Holter Vogel <44628526+kylehovo@users.noreply.github.com>
2026-06-24 10:21:42 -0500

    Make cookie deletion description more descriptive (no fun allowed)

--- internal/config/routes/routes.go
+++ internal/config/routes/routes.go
@@ -124,7 +124,7 @@ func CreateMux(services Services) MuxWrapper {
 							Handler: c.DeleteCookie(),
 							Responses: map[int]Response{
 								204: {
-									Description: "yeag",
+									Description: "Deleted cookie successfully",
 								},
 							},
 						},
